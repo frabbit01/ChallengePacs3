6d48a233eb4436ef55fd6562b42d5bca4b678286
frabbit01 <francesca.visalli@mail.polimi.it>
2024-05-21 10:29:37 +0000

    remember to modify in order to use the correct value of pi in the function

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@ static double c_start, c_diff;
     std::cout << x << c_diff << " [s]" << std::endl; \
   }
 
-int main(){
+int main(int argc, char** argv){
+    int provided;
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    MPI_Finalize();
     return 0;
 }
