878b58d3f3bd6e78e243d0bb230b156a43b85eb4
frabbit01 <francesca.visalli@mail.polimi.it>
2024-05-21 09:59:17 +0000

    main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdio.h>
+#include<mpi.h>
+#include<omp.h>
+
+static double c_start, c_diff;
+#define tic() c_start = MPI_Wtime();
+#define toc(x)                                       \
+  {                                                  \
+    c_diff = MPI_Wtime() - c_start;                  \
+    std::cout << x << c_diff << " [s]" << std::endl; \
+  }
+
+int main(){
+    return 0;
+}
